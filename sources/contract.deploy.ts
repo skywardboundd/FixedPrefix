aafa1ca716a2a23f87bedf0817365fe4bb21f45e
b4cksl4sh <andrei.sh@ton.org>
2025-04-15 20:59:34 +0300

    Done

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,15 +1,16 @@
 import * as fs from "fs";
 import * as path from "path";
-import { Address, contractAddress } from "@ton/core";
+import { Address, beginCell, contractAddress } from "@ton/core";
 import { SampleTactContract } from "./output/sample_SampleTactContract";
 import { prepareTactDeployment } from "@tact-lang/deployer";
+import { JettonMinter } from "./output/Jetton_JettonMinter";
 
 (async () => {
     // Parameters
     let testnet = true;
-    let packageName = "sample_SampleTactContract.pkg";
+    let packageName = "Jetton_JettonMinter.pkg";
     let owner = Address.parse("kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB");
-    let init = await SampleTactContract.init(owner);
+    let init = await JettonMinter.init(owner, beginCell().endCell());
 
     // Load required data
     let address = contractAddress(0, init);
--- sources/contract.spec.ts
+++ sources/contract.spec.ts
@@ -1,51 +1,104 @@
-import { beginCell, toNano } from "@ton/core";
-import { Blockchain } from "@ton/sandbox";
+import { Address, Cell, toNano } from "@ton/core";
+import { Blockchain, SandboxContract } from "@ton/sandbox";
 import "@ton/test-utils";
-import { SampleTactContract } from "./output/sample_SampleTactContract";
-import { findErrorCodeByMessage } from './utils/error';
-import { flattenTransaction } from "@ton/test-utils";
+import { Parent } from "./output/sample_Parent";
+import { Child } from "./output/sample_Child";
+import { OptimizedParent } from "./output/sample_OptimizedParent";
+
+const calculateCellsAndBits = (
+    root: Cell,
+    visited: Set<string> = new Set<string>(),
+) => {
+    const hash = root.hash().toString("hex");
+    if (visited.has(hash)) {
+        return { cells: 0, bits: 0 };
+    }
+    visited.add(hash);
+
+    let cells = 1;
+    let bits = root.bits.length;
+    for (const ref of root.refs) {
+        const childRes = calculateCellsAndBits(ref, visited);
+        cells += childRes.cells;
+        bits += childRes.bits;
+    }
+    return { cells, bits };
+};
+
+export async function getStateSizeForAccount(
+    blockchain: Blockchain,
+    address: Address,
+): Promise<{ cells: number; bits: number }> {
+    const accountState = (await blockchain.getContract(address)).accountState;
+    if (!accountState || accountState.type !== "active") {
+        throw new Error("Account state not found");
+    }
+    if (!accountState.state.code || !accountState.state.data) {
+        throw new Error("Account state code or data not found");
+    }
+    const accountCode = accountState.state.code;
+    const accountData = accountState.state.data;
+
+    const codeSize = calculateCellsAndBits(accountCode);
+    const dataSize = calculateCellsAndBits(accountData);
+
+    return {
+        cells: codeSize.cells + dataSize.cells,
+        bits: codeSize.bits + dataSize.bits,
+    };
+}
 
 describe("contract", () => {
+	let blockchain: Blockchain;
+	let owner: any;
+	let parentContract: SandboxContract<Parent>;
+	let usualParentChild: SandboxContract<Child>;
+	let optimizedParentChild: SandboxContract<Child>;
+	let optimizedParentContract: SandboxContract<OptimizedParent>;
     it("should deploy correctly", async () => {
-        const temp2 = Blockchain.create;
-        Blockchain.create = async (...args) => {
-            const system = await temp2(...args);
-            system.verbosity.vmLogs = "vm_logs_verbose";
-            return system;
-        }
-        let system = await Blockchain.create();
-
-        let owner = await system.treasury("deployer");
-        let nonOwner = await system.treasury("non-owner");
-
-        let contract = system.openContract(await SampleTactContract.fromInit());
-        const slice = beginCell().storeUint(0, 32).asSlice();
-        await contract.send(owner.getSender(), { value: toNano(1), bounce: false }, slice);
-
-        const balanceBefore = await (await system.getContract(contract.address)).balance;
-        const deployResult = await contract.send(owner.getSender(), { value: toNano(1.7) }, slice);
-        const balanceAfter = await (await system.getContract(contract.address)).balance;
+        // Create Sandbox and deploy contract
+        blockchain = await Blockchain.create();
+		//blockchain.verbosity.vmLogs = "vm_logs_verbose";
+        owner = await blockchain.treasury("owner");
+        parentContract = blockchain.openContract(await Parent.fromInit());
+		usualParentChild = blockchain.openContract(await Child.fromInit(parentContract.address));
+		optimizedParentContract = blockchain.openContract(await OptimizedParent.fromInit());
+		optimizedParentChild = blockchain.openContract(await Child.fromInit(optimizedParentContract.address));
+		
+        const deployResult = await parentContract.send(owner.getSender(), { value: toNano(1) }, null);
         expect(deployResult.transactions).toHaveTransaction({
             from: owner.address,
-            to: contract.address,
+            to: parentContract.address,
+            deploy: true,
             success: true,
         });
-        expect(deployResult.transactions).toHaveTransaction({
-            from: contract.address,
-            to: owner.address,
-            value: toNano(1),
-        })
-        expect(deployResult.transactions).toHaveTransaction({
-            from: contract.address,
-            to: owner.address,
-            value: (value) => { return value != undefined && value > toNano(0.5) }
-        })
-        expect(balanceAfter).toEqual(balanceAfter);
-        console.warn(balanceBefore);
-        console.warn(balanceAfter);
 
-        console.log(flattenTransaction(deployResult.transactions[1]));
-        console.log(flattenTransaction(deployResult.transactions[2]));
-        console.log(flattenTransaction(deployResult.transactions[3]));
+		const deployResultOptimized = await optimizedParentContract.send(owner.getSender(), { value: toNano(1) }, null);
+		expect(deployResultOptimized.transactions).toHaveTransaction({
+			from: owner.address,
+			to: optimizedParentContract.address,
+			deploy: true,
+			success: true,
+		});
     });
+    it("Usual parent should work correctly", async () => {
+        const res = await usualParentChild.send(owner.getSender(), { value: toNano(1) }, null);
+		expect(res.transactions).toHaveTransaction({
+			from: parentContract.address,
+			to: Address.parse("UQBAmIBdInKmGzdTUMay9fqq8nyCZ9jnUh_yBFEE_cfediVD"),
+		});
+		const storageStats = await getStateSizeForAccount(blockchain, parentContract.address);
+		console.log("Version without optmization");
+		console.log(storageStats);
+    });
+	it("Optimized parent should work correctly", async () => {
+		const res = await optimizedParentChild.send(owner.getSender(), { value: toNano(1) }, null);
+		expect(res.transactions).toHaveTransaction({
+			from: optimizedParentContract.address,
+			to: Address.parse("UQBAmIBdInKmGzdTUMay9fqq8nyCZ9jnUh_yBFEE_cfediVD"),
+		});
+		const storageStats = await getStateSizeForAccount(blockchain, optimizedParentContract.address);
+		console.log("Version with optmization");
+		console.log(storageStats);
+	});
 });
